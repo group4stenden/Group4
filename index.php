85b52bc3808acfd60d1737780949048ac5d244c1
Tom <tamas.eckert@student.nhlstenden.com>
2018-09-26 13:30:56 +0200

    Added HTML and CSS files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Weather</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <?php
+    // code here
+    ?>
+</body>
+</html>
